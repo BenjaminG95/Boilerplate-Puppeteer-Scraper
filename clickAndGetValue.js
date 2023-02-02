3a863c98f510cc568ada5b8beb44c93ac862c602
Benjamin <Galoisb95@gmail.com>
2023-02-02 01:35:51 +0100

    add clickAndGetValue.js

---
+++ clickAndGetValue.js
@@ -0,0 +1,28 @@
+/**
+ * @name clickAndGetValue
+ *
+ * @desc Clicks on an element and retrieves its value
+ *
+ */
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+    // Launch a browser in non-headless mode
+    const browser = await puppeteer.launch({headless: false});
+    // Open a new page
+    const page = await browser.newPage();
+
+    // Go to a website
+    await page.goto('https://www.infowebmaster.fr/outils/generateur-nombre-aleatoire.php');
+    // Click on an element
+    await page.click('#content > form > fieldset > div.centrer > input[type=button]');
+
+    // Retrieve the value of an element and store it in a variable
+    let result = await page.evaluate(() => document.querySelector('#result').innerText);
+    // Log the value to the console
+    console.log(result)
+
+    // Close the browser
+    // await browser.close();
+})()
